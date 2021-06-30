f672953877a52d8e88749f4746ee9aa2d266aafc
John <John@DESKTOP-INNVISM>
2021-07-01 01:36:47 +0900

    test

--- src/cho_calander/Calander.java
+++ src/cho_calander/Calander.java
@@ -1,5 +1,7 @@
 package cho_calander;
 
+import java.util.Scanner;
+
 public class Calander {
 	public static void main(String[] args) {
 		System.out.println(" 일 월  화  수 목  금 토");
@@ -8,5 +10,13 @@ public class Calander {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		System.out.printf("%d월은 %d일까지 있습니다.",month,arr[month-1]);
+		
+		scanner.close();
 	}
 }
