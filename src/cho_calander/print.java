0a3124e7ce571fe34adc10b0d4708d2b6cda1016
John <John@DESKTOP-INNVISM>
2021-07-01 12:21:58 +0900

    add prompt

--- src/cho_calander/print.java
+++ src/cho_calander/print.java
@@ -5,17 +5,21 @@ public class print {
 	public static void main(String[] args) {
 		int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("반복횟수를 입력하세요.");
-		int num = scanner.nextInt();
-		System.out.println("월을 입력하세요.");
-		int[] input = new int[num];
-		for(int i = 0; i < num; i++) {	
-			int month = scanner.nextInt();
-			input[i] = month;
+		int month = 1;
+		while(true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print("> ");
+			month = scanner.nextInt();
+			if(month == -1) {
+				break;
+			}
+			else if(month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다.\n",month,arr[month-1]);
 		}
-		for(int i = 0; i< num; i++) {
-		System.out.printf("%d월은 %d일까지 있습니다.\n",input[i],arr[input[i]-1]);
-		}
+		
+		System.out.println("Have a nice day!");
 		scanner.close();
 	}
 }
