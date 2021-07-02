6b1e7e80820b3468d195f3b3b6903fe567ac0e2f
John <John@DESKTOP-INNVISM>
2021-07-02 17:51:43 +0900

    add print real calander

--- src/cho_calander/Calander.java
+++ src/cho_calander/Calander.java
@@ -1,7 +1,7 @@
 package cho_calander;
 
 public class Calander {
-	public static void show_calander(int year, int month, int weekday){
+	public static void show_calander(int year, int month){
 		System.out.printf("     <<%4d %d>>\n",year,month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
@@ -11,15 +11,16 @@ public class Calander {
 				maxday += 1;
 			}
 		}
-	
-	if(weekday != 0) {
-	for(int i=0; i< 7-weekday; i++) {
-	System.out.printf("   ");
-	}
-	}
+		int weekday = calculate_day(year, month);
+		
+		if(weekday != 0) {
+			for(int i=0; i< weekday; i++) {
+			System.out.printf("   ");
+			}
+		}
 		for(int i=1; i<=maxday; i++) {
 			System.out.printf("%3d",i);
-			if(i % 7 == weekday) {
+			if(i % 7 == 7-weekday) {
 				System.out.println();
 			}
 			
@@ -32,4 +33,20 @@ public class Calander {
 		return arr[month-1];
 	}
 	
+	public static int calculate_day(int year, int month) {
+		int[] arr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		if((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0 ) {
+			arr[1] = 29;
+		}
+		
+		year--;
+		
+		int totaldays = (365*year) + (year/4) - (year/100) + (year / 400);
+		
+		for(int i=0; i<month-1; i++) {
+			totaldays += arr[i];
+		}
+		totaldays++;
+		return (totaldays%7);
+	}
 }
--- src/cho_calander/Prompt.java
+++ src/cho_calander/Prompt.java
@@ -3,31 +3,7 @@ package cho_calander;
 import java.util.Scanner;
 
 public class Prompt {
-	public int parseDay(String weekday) {
-		if(weekday.equals("SU")) {
-			return 0;
-		}
-		else if(weekday.equals("MO")) {
-			return 6;
-		}
-		else if(weekday.equals("TU")) {
-			return 5;
-		}
-		else if(weekday.equals("WE")) {
-			return 4;
-		}
-		else if(weekday.equals("TH")) {
-			return 3;
-		}
-		else if(weekday.equals("FR")) {
-			return 2;
-		}
-		else if(weekday.equals("SA")) {
-			return 1;
-		}
-		else 
-			return 0;
-	}
+	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
@@ -37,16 +13,13 @@ public class Prompt {
 			System.out.println("월을 입력하세요.");
 			System.out.print("MONTH> ");
 			int month = scanner.nextInt();
-			System.out.println("첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)");
-			System.out.print("WEEKDAY> ");
-			String weekday = scanner.next();
 			
 			if(month == -1) {
 				System.out.println("Bye~");
 				break;
 			}
-			int int_weekday = parseDay(weekday);
-			Calander.show_calander(year,month,int_weekday);
+			
+			Calander.show_calander(year,month);
 		}
 		scanner.close();
 	}
@@ -54,9 +27,6 @@ public class Prompt {
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
-		
-		
-	
 	}
 
 }
