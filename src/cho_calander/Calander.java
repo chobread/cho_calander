c823c4e53fd3b6d91e2f5ac42ed4ada8cfa4a381
John <John@DESKTOP-INNVISM>
2021-07-03 03:02:36 +0900

    add functions(add, change, find schedule)

--- src/cho_calander/Calander.java
+++ src/cho_calander/Calander.java
@@ -1,5 +1,10 @@
 package cho_calander;
 
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Map;
+
 public class Calander {
 	public static void show_calander(int year, int month){
 		System.out.printf("     <<%4d %d>>\n",year,month);
@@ -26,6 +31,78 @@ public class Calander {
 			
 		}
 		System.out.println();
+	}
+	
+
+	public static void show_present_calender(Map<String,ArrayList<String>> map) {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		
+		String sample_year = Integer.toString(year);
+		String sample_month = Integer.toString(month);
+		if(month < 10) {
+			sample_month = "0" + sample_month;
+		}
+			
+		
+		System.out.printf("     <<%4d %d>>\n",year,month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		
+		int maxday = getMaxMonth(month);
+		if(month == 2) {
+			if((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0 ) {
+				maxday += 1;
+			}
+		}
+		int weekday = calculate_day(year, month);
+		
+		if(weekday != 0) {
+			for(int i=0; i< weekday; i++) {
+			System.out.printf("   ");
+			}
+		}
+		for(int i = 1; i<=maxday; i++) {
+			System.out.printf("%3d",i);
+			if(i % 7 == 7-weekday) {
+				System.out.println();
+				if(i == 7-weekday) {
+					for(int k = 0; k < weekday; k++) {
+					System.out.printf("   ");
+					}
+					for(int m = 1; m <= i; m++) {
+						String sample_day = "0"+Integer.toString(m);
+						String sample_key =  sample_year + "-" + sample_month + "-" + sample_day;
+						if(map.containsKey(sample_key)) {
+							System.out.printf("  .");
+						}
+						else {
+							System.out.printf("   ");
+						}
+					}
+				}
+				else {
+				for(int j = i-6; j <= i; j++) {
+					String sample_day = Integer.toString(j);
+					if(j<10) {
+						sample_day = "0" + sample_day;
+					}
+					String sample_key =  sample_year + "-" + sample_month + "-" + sample_day;
+					if(map.containsKey(sample_key)) {
+						System.out.printf("  .");
+					}
+					else {
+						System.out.printf("   ");
+					}
+				}
+				}
+				System.out.println();
+			}
+		
+		}
+		System.out.println();
+		
 	}
 	
 	public static int getMaxMonth(int month) {
--- src/cho_calander/Prompt.java
+++ src/cho_calander/Prompt.java
@@ -1,25 +1,148 @@
 package cho_calander;
 
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Prompt {
+	Scanner scanner = new Scanner(System.in);
+	Map<String,ArrayList<String>> map = new HashMap<>();
 	
+	
+	public void add_schedule() {
+		
+		System.out.println("[일정 등록] 날짜를 입력하세요.(ex 2021-02-13)");
+		System.out.print("> ");
+		String date = scanner.next();
+		scanner.nextLine();
+		System.out.println("일정을 입력하세요.");
+		System.out.print("> ");
+		
+		String Schedule = scanner.nextLine();
+		
+		ArrayList<String> arr_schedule;
+		
+		if(map.containsKey(date)) {
+			arr_schedule = map.get(date);
+			arr_schedule.add(Schedule);
+		}
+		else {
+			arr_schedule = new ArrayList<>();
+			arr_schedule.add(Schedule);
+		}
+		
+		map.put(date,arr_schedule);
+		
+		System.out.println("일정이 등록되었습니다.");
+		
+		
+	}
+	
+	void see_schedule(){
+		
+		System.out.println("[일정 검색] 날짜를 입력하세요.");
+		System.out.print("> ");
+		
+		String date = scanner.next();
+		ArrayList<String> arr_schedule = map.get(date);
+		
+		for(int i = 0; i < arr_schedule.size(); i++) {
+			System.out.println(arr_schedule.get(i));
+		}
+		
+		
+	}
+	
+	void change_schedule() {
+		System.out.println("[일정 변경] 날짜를 입력하세요.");
+		System.out.print("> ");
+		String date = scanner.next();
+		
+		System.out.println("[일정 변경] 변경할 일정의 인덱스를 입력하세요.");
+		System.out.print("> ");
+		int index = scanner.nextInt();
+		
+		System.out.println("[일정 변경] 변경할 일정을 입력하세요.");
+		System.out.print("> ");
+		String Schedule = scanner.next();
+		
+		ArrayList<String> arr_schedule;
+		arr_schedule = map.get(date);
+		arr_schedule.set(index, Schedule);
+		
+		map.put(date, arr_schedule);
+		System.out.println("[일정 변경] 변경이 완료됐습니다.");
+		
+	}
+	
+	void today_schedule() {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		
+		String sample_year = Integer.toString(year);
+		String sample_month = Integer.toString(month);
+		if(month < 10) {
+			sample_month = "0" + sample_month;
+		}
+		String sample_day = Integer.toString(day);
+		if(day < 10) {
+			sample_day = "0" + sample_day;
+		}
+		String sample_key =  sample_year + "-" + sample_month + "-" + sample_day;
+		
+		
+		ArrayList<String> arr_schedule = map.get(sample_key);
+		System.out.println("오늘의 일정입니다");
+		for(int i = 0; i < arr_schedule.size(); i++) {
+			System.out.println(arr_schedule.get(i));
+		}
+		
+		
+	}
 	public void runPrompt() {
-		Scanner scanner = new Scanner(System.in);
+	
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 일정 변경");
+		System.out.println("| 4. 달력 보기");
+		System.out.println("| 5. 오늘의 일정 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+		
+		
 		while(true) {
-			System.out.println("년도를 입력하세요.");
-			System.out.print("YEAR> ");
-			int year = scanner.nextInt();
-			System.out.println("월을 입력하세요.");
-			System.out.print("MONTH> ");
-			int month = scanner.nextInt();
-			
-			if(month == -1) {
-				System.out.println("Bye~");
+			System.out.println("명령 (1, 2, 3, 4, 5, h, q)");
+			System.out.print("> ");
+			String input = scanner.next();
+			if(input.equals("1")) {
+				add_schedule();
+			}
+			else if(input.equals("2")) {
+				see_schedule();
+			}
+			else if(input.equals("3")) {
+				change_schedule();
+			}
+			else if(input.equals("4")) {
+				Calander.show_present_calender(map);
+			}
+			else if(input.equals("5")) {
+				today_schedule();
+			}
+			else if(input.equals("h")) {
+				System.out.println("도움말 입니다.");
+			}
+			else if(input.equals("q")) {
+				System.out.println("Bye");
 				break;
 			}
 			
-			Calander.show_calander(year,month);
+		
 		}
 		scanner.close();
 	}
