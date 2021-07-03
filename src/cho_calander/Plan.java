bbd86602c46b30b3603dc83929a910541f2392ec
John <cjfghks223991@gmail.com>
2021-07-03 17:32:09 +0900

    refactoring code

---
+++ src/cho_calander/Plan.java
@@ -0,0 +1,104 @@
+package cho_calander;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+public class Plan {
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void add_schedule(Map<String,ArrayList<String>> map) {
+		
+		System.out.println("[일정 등록] 날짜를 입력하세요.(ex 2021-02-13)");
+		System.out.print("> ");
+		String date = scanner.next();
+		scanner.nextLine();
+		System.out.println("일정을 입력하세요.");
+		System.out.print("> ");
+		
+		String Schedule = scanner.nextLine();
+		
+		ArrayList<String> arr_schedule;
+		
+		if(map.containsKey(date)) {
+			arr_schedule = map.get(date);
+			arr_schedule.add(Schedule);
+		}
+		else {
+			arr_schedule = new ArrayList<>();
+			arr_schedule.add(Schedule);
+		}
+		
+		map.put(date,arr_schedule);
+		
+		System.out.println("일정이 등록되었습니다.");
+		
+		
+	}
+	 static void see_schedule(Map<String,ArrayList<String>> map){
+		
+		System.out.println("[일정 검색] 날짜를 입력하세요.");
+		System.out.print("> ");
+		
+		String date = scanner.next();
+		ArrayList<String> arr_schedule = map.get(date);
+		
+		for(int i = 0; i < arr_schedule.size(); i++) {
+			System.out.println(arr_schedule.get(i));
+		}
+		
+		
+	}
+	 static void change_schedule(Map<String,ArrayList<String>> map) {
+		System.out.println("[일정 변경] 날짜를 입력하세요.");
+		System.out.print("> ");
+		String date = scanner.next();
+		
+		System.out.println("[일정 변경] 변경할 일정의 인덱스를 입력하세요.");
+		System.out.print("> ");
+		int index = scanner.nextInt();
+		
+		System.out.println("[일정 변경] 변경할 일정을 입력하세요.");
+		System.out.print("> ");
+		String Schedule = scanner.next();
+		
+		ArrayList<String> arr_schedule;
+		arr_schedule = map.get(date);
+		arr_schedule.set(index, Schedule);
+		
+		map.put(date, arr_schedule);
+		System.out.println("[일정 변경] 변경이 완료됐습니다.");
+		
+	}
+	
+	public static void today_schedule(Map<String,ArrayList<String>> map) {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		
+		String sample_year = Integer.toString(year);
+		String sample_month = Integer.toString(month);
+		if(month < 10) {
+			sample_month = "0" + sample_month;
+		}
+		String sample_day = Integer.toString(day);
+		if(day < 10) {
+			sample_day = "0" + sample_day;
+		}
+		String sample_key =  sample_year + "-" + sample_month + "-" + sample_day;
+		
+		
+		ArrayList<String> arr_schedule = map.get(sample_key);
+		System.out.println("오늘의 일정입니다");
+		for(int i = 0; i < arr_schedule.size(); i++) {
+			System.out.println(arr_schedule.get(i));
+		}
+		
+		
+	}
+}
--- src/cho_calander/Prompt.java
+++ src/cho_calander/Prompt.java
@@ -11,98 +11,11 @@ public class Prompt {
 	Map<String,ArrayList<String>> map = new HashMap<>();
 	
 	
-	public void add_schedule() {
-		
-		System.out.println("[일정 등록] 날짜를 입력하세요.(ex 2021-02-13)");
-		System.out.print("> ");
-		String date = scanner.next();
-		scanner.nextLine();
-		System.out.println("일정을 입력하세요.");
-		System.out.print("> ");
-		
-		String Schedule = scanner.nextLine();
-		
-		ArrayList<String> arr_schedule;
-		
-		if(map.containsKey(date)) {
-			arr_schedule = map.get(date);
-			arr_schedule.add(Schedule);
-		}
-		else {
-			arr_schedule = new ArrayList<>();
-			arr_schedule.add(Schedule);
-		}
-		
-		map.put(date,arr_schedule);
-		
-		System.out.println("일정이 등록되었습니다.");
-		
-		
-	}
 	
-	void see_schedule(){
-		
-		System.out.println("[일정 검색] 날짜를 입력하세요.");
-		System.out.print("> ");
-		
-		String date = scanner.next();
-		ArrayList<String> arr_schedule = map.get(date);
-		
-		for(int i = 0; i < arr_schedule.size(); i++) {
-			System.out.println(arr_schedule.get(i));
-		}
-		
-		
-	}
 	
-	void change_schedule() {
-		System.out.println("[일정 변경] 날짜를 입력하세요.");
-		System.out.print("> ");
-		String date = scanner.next();
-		
-		System.out.println("[일정 변경] 변경할 일정의 인덱스를 입력하세요.");
-		System.out.print("> ");
-		int index = scanner.nextInt();
-		
-		System.out.println("[일정 변경] 변경할 일정을 입력하세요.");
-		System.out.print("> ");
-		String Schedule = scanner.next();
-		
-		ArrayList<String> arr_schedule;
-		arr_schedule = map.get(date);
-		arr_schedule.set(index, Schedule);
-		
-		map.put(date, arr_schedule);
-		System.out.println("[일정 변경] 변경이 완료됐습니다.");
-		
-	}
 	
-	void today_schedule() {
-		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
-		int month = cal.get(Calendar.MONTH) + 1;
-		int day = cal.get(Calendar.DAY_OF_MONTH);
-		
-		String sample_year = Integer.toString(year);
-		String sample_month = Integer.toString(month);
-		if(month < 10) {
-			sample_month = "0" + sample_month;
-		}
-		String sample_day = Integer.toString(day);
-		if(day < 10) {
-			sample_day = "0" + sample_day;
-		}
-		String sample_key =  sample_year + "-" + sample_month + "-" + sample_day;
-		
-		
-		ArrayList<String> arr_schedule = map.get(sample_key);
-		System.out.println("오늘의 일정입니다");
-		for(int i = 0; i < arr_schedule.size(); i++) {
-			System.out.println(arr_schedule.get(i));
-		}
-		
-		
-	}
+	
+	
 	public void runPrompt() {
 	
 		System.out.println("+----------------------+");
@@ -114,38 +27,37 @@ public class Prompt {
 		System.out.println("| h. 도움말 q. 종료");
 		System.out.println("+----------------------+");
 		
+		boolean flag = false;
 		
 		while(true) {
 			System.out.println("명령 (1, 2, 3, 4, 5, h, q)");
 			System.out.print("> ");
 			String input = scanner.next();
-			if(input.equals("1")) {
-				add_schedule();
+			switch(input){
+				case "1":
+					Plan.add_schedule(map); break;
+				case "2":
+					Plan.see_schedule(map); break;
+				case "3":
+					Plan.change_schedule(map); break;
+				case "4":
+					Calander.show_present_calender(map); break;
+				case "5":
+					Plan.today_schedule(map); break;
+				case "h":
+					System.out.println("도움말 입니다."); break;
+				case "q":
+					System.out.println("Bye");
+					flag = true;
+					break; 		
 			}
-			else if(input.equals("2")) {
-				see_schedule();
-			}
-			else if(input.equals("3")) {
-				change_schedule();
-			}
-			else if(input.equals("4")) {
-				Calander.show_present_calender(map);
-			}
-			else if(input.equals("5")) {
-				today_schedule();
-			}
-			else if(input.equals("h")) {
-				System.out.println("도움말 입니다.");
-			}
-			else if(input.equals("q")) {
-				System.out.println("Bye");
+			if(flag) {
 				break;
 			}
-			
 		
 		}
-		scanner.close();
 	}
+	
 	
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
